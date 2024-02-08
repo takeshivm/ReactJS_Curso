945572ff17f93ae7f3fbb4a32570034fd3dfcf47
takeshivm <vargasmurillotakeshi@gmail.com>
2024-02-08 12:59:04 -0500

    #4, Configuraciones del backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,24 @@
+// npm run dev para inciar el servidor
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// settings
+app.set('port', process.env.PORT || 4000); 
+
+
+
+// middlewares
+app.use(cors());
+app.use(express.json()); // Con esto el servidor soportará formatos json y strings
+
+
+
+// routes
+app.get('/users', (req, res) => res.send('Users routes'));
+app.get('/notes', (req, res) => res.send('Notes routes'));
+
+
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ const app = require('./app');
 require('./database');
 
 async function main() {
-    await app.listen(4000)
+    //await app.listen(4000)
+    await app.listen(app.get('port'))
     console.log('Server on port 4000');    
 }
 
